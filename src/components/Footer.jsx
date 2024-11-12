def7fd556ee16a448c8f5a48c19e43f5759ee801
Zuruel Kamande <zurueladrian@gmail.com>
2024-11-12 11:49:00 +0300

    adds

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <div className="footer">
+           <div>
+            <p><strong>All Rights Reserved</strong></p>
+            <p>2024</p>
+            <p><span>Group VI</span></p>
+           </div>
+        </div>
+    )
+}
